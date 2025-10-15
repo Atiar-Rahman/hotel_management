ff05d293cf2ba2beb2275ff78a43e01cc4fe188a
Atiar-Rahman <atiar1709@gmail.com>
2025-10-15 14:34:43 +0600

    email ativation update

--- staybangla/settings.py
+++ staybangla/settings.py
@@ -150,8 +150,8 @@ SWAGGER_SETTINGS = {
 }
 # Djoser
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}/',
+    'ACTIVATION_URL': 'activate/{uid}/{token}/',
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
         'user_create':'users.serializers.UserCreateSerializer',
