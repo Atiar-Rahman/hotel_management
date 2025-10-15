8112ac623c9c7748d4fac1dc4ecb96a87014adcd
Atiar-Rahman <atiar1709@gmail.com>
2025-10-15 15:37:09 +0600

    djoser setting update

--- staybangla/settings.py
+++ staybangla/settings.py
@@ -150,7 +150,8 @@ SWAGGER_SETTINGS = {
 }
 # Djoser
 DJOSER = {
-    'EMAIL_FRONTEND_PROTOCOL':config('FRONTEND_URL'),
+    'EMAIL_FRONTEND_PROTOCOL': config('FRONTEND_PROTOCOL'),
+    'EMAIL_FRONTEND_DOMAIN': config('FRONTEND_DOMAIN'),
     'EMAIL_FRONTEND_SITE_NAME':'staybangla',
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}/',
     'ACTIVATION_URL': 'activate/{uid}/{token}/',
